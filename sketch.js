5d747696f150ff7354454eec62484b429d258891
alessiamattesini <60142505+alessiamattesini@users.noreply.github.com>
2020-10-06 21:45:38 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,10 +3,39 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight)
-  // put setup code here
+  createCanvas(windowWidth,windowHeight);
+  noFill();
 }
 
 function draw() {
-  // put drawing code here
+
+  //testo
+  push();
+  textAlign(CENTER);
+  noStroke();
+  textSize(30);
+  var colore = color('white');
+  colore.setAlpha(4000 - millis());
+  fill(colore);
+  text("Move your mouse to create", 0, 120, width);
+  pop();
+
+  background(10, 10); // translucent background
+
+    const colori = ['#FFD639',
+               	'#b33c86',
+               	'#36f1cd',
+                '#acf39d'];
+  const colorHex = random(colori);
+  stroke(color(colorHex));
+  //grid of circles
+  for (let x = -100; x <= (width+100); x += 45) {
+    for (let y = -100; y <= (height+100); y += 35){
+      //moving the grid with the mouse
+    let pipox = map(mouseX, 0, width, 0, 100);
+    let pippox = x+pipox;
+    let pipoy = map(mouseY, 0, height, 0, 100);
+    let pippoy = y+pipoy;
+  ellipse(pippox, pippoy, 15);}
+}
 }
